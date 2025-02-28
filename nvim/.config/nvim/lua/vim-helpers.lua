56374f24280634f1af07128e9649deb9cf20080a
kunkka19xx <nobita079x@gmail.com>
2025-02-28 18:33:36 +0900

    push the current branch

--- nvim/.config/nvim/lua/vim-helpers.lua
+++ nvim/.config/nvim/lua/vim-helpers.lua
@@ -22,3 +22,13 @@ vim.keymap.set("n", "<leader>cp", function()
 	vim.fn.system("echo '" .. filepath .. "' | pbcopy") -- Copy to macOS clipboard
 	print("Copied: " .. filepath)
 end, { desc = "Copy absolute path to clipboard" })
+-- open the current file in browser
+vim.keymap.set("n", "<leader>ob", function()
+  local file_path = vim.fn.expand("%:p") -- get the current file path
+  if file_path ~= "" then
+    local cmd = vim.fn.has("mac") == 1 and "open " .. file_path or "xdg-open " .. file_path
+    os.execute(cmd .. " &")
+  else
+    print("No file to open")
+  end
+end, { desc = "Open current file in browser" })
