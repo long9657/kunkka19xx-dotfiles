f275b367a1630e33b86c9a99ec8388ffa2c50e8c
kunkka19xx <nobita079x@gmail.com>
2025-05-10 13:33:35 +0900

    nicer oil, fzf

--- nvim/.config/nvim/lua/plugins/java.lua
+++ nvim/.config/nvim/lua/plugins/java.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-java/nvim-java",
 	config = function()
 		require("java").setup()
-		require("lspconfig").jdtls.setup({})
+		-- require("lspconfig").jdtls.setup({})
 	end,
 }
 -- https://github.com/nvim-java/nvim-java?tab=readme-ov-file
