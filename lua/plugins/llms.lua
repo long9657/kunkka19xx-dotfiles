d0c4290d9aa93387ad04d9640b9a051ea029012b
kunkka19xx <nobita079x@gmail.com>
2026-03-21 08:59:35 +0900

    Squashed 'nvim/.config/nvim/' changes from 1e95659..944e31c
    944e31c adjust json reading
    c803595 testing websocket rq/rs
    2e2aded sync surface nix, add opencode
    34b6867 rest in nvim
    b2e80c6 sync
    5fc3bfb sync
    f4aafc0 opencode
    
    git-subtree-dir: nvim/.config/nvim
    git-subtree-split: 944e31c3951d035ba5a9be9cab25a000802d2df9

--- lua/plugins/llms.lua
+++ lua/plugins/llms.lua
@@ -65,7 +65,7 @@ return {
         config = function()
             require("supermaven-nvim").setup({
                 keymaps = {
-                    accept_suggestion = "<Tab>",
+                    accept_suggestion = "<C-y>",
                     clear_suggestion = "<C-]>",
                     accept_word = "<C-j>",
                 },
